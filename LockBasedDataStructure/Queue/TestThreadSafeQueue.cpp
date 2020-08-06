b3eaf95e96cf3ccf4f3bcc589c5c77744eacd67f
ho-ri1991 <hage.1991@hotmail.co.jp>
2020-08-06 23:13:51 +0900

    add ThreadSafeQueue

--- LockBasedDataStructure/Queue/TestThreadSafeQueue.cpp
+++ LockBasedDataStructure/Queue/TestThreadSafeQueue.cpp
@@ -37,7 +37,275 @@ public:
   }
 };
 
-BOOST_AUTO_TEST_CASE(TestThreadSafeQueue)
+BOOST_AUTO_TEST_CASE(TestThreadSafeQueueWaitAndPop)
 {
+  ThreadSafeQueue<std::pair<int, int>> queue;
+  std::vector<std::pair<int, int>> data1;
+  std::vector<std::pair<int, int>> data2;
+  {
+    jthread push1([&queue](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(int i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        queue.push(std::make_pair(0, i));
+      }
+    });
+    jthread push2([&queue](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(int i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        queue.push(std::make_pair(1, i));
+      }
+    });
+    jthread pop1([&queue, &data1](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(std::size_t i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        data1.push_back(*queue.waitAndPop());
+      }
+    });
+    jthread pop2([&queue, &data2](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(std::size_t i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        data2.push_back(*queue.waitAndPop());
+      }
+    });
+  }
+  {
+    std::vector<int> arr1, arr2;
+    for(auto& [n, i]: data1)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(auto& [n, i]: data2)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    std::sort(arr1.begin(), arr1.end());
+    std::sort(arr2.begin(), arr2.end());
+    std::vector<int> expected;
+    for(std::size_t i = 0; i < 100; ++i)
+    {
+      expected.push_back(i);
+    }
+    BOOST_CHECK_EQUAL_COLLECTIONS(arr1.begin(), arr1.end(), expected.begin(), expected.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(arr2.begin(), arr2.end(), expected.begin(), expected.end());
+  }
+  {
+    std::vector<int> arr1, arr2;
+    for(auto& [n, i]: data1)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(int i = 0; i < (int)arr1.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr1[i] < arr1[i + 1]);
+    }
+    for(int i = 0; i < (int)arr2.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr2[i] < arr2[i + 1]);
+    }
+    arr1.clear();
+    arr2.clear();
+    for(auto& [n, i]: data2)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(int i = 0; i < (int)arr1.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr1[i] < arr1[i + 1]);
+    }
+    for(int i = 0; i < (int)arr2.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr2[i] < arr2[i + 1]);
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestThreadSafeQueuetryPop)
+{
+  ThreadSafeQueue<std::pair<int, int>> queue;
+  std::vector<std::pair<int, int>> data1;
+  std::vector<std::pair<int, int>> data2;
+  {
+    jthread push1([&queue](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(int i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        queue.push(std::make_pair(0, i));
+      }
+    });
+    jthread push2([&queue](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(int i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        queue.push(std::make_pair(1, i));
+      }
+    });
+    jthread pop1([&queue, &data1](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(std::size_t i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        std::shared_ptr<std::pair<int, int>> p;
+        while(!(p = queue.tryPop()));
+        data1.push_back(*p);
+      }
+    });
+    jthread pop2([&queue, &data2](){
+      std::random_device rnd;
+      std::mt19937 engine(rnd());
+      std::uniform_int_distribution<> dist(0, 10);
+      using namespace std::literals::chrono_literals;
+      std::this_thread::sleep_for(1ms);
+      for(std::size_t i = 0; i < 100; ++i)
+      {
+        std::this_thread::sleep_for(std::chrono::microseconds(dist(engine)));
+        std::shared_ptr<std::pair<int, int>> p;
+        while(!(p = queue.tryPop()));
+        data2.push_back(*p);
+      }
+    });
+  }
+  {
+    std::vector<int> arr1, arr2;
+    for(auto& [n, i]: data1)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(auto& [n, i]: data2)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    std::sort(arr1.begin(), arr1.end());
+    std::sort(arr2.begin(), arr2.end());
+    std::vector<int> expected;
+    for(std::size_t i = 0; i < 100; ++i)
+    {
+      expected.push_back(i);
+    }
+    BOOST_CHECK_EQUAL_COLLECTIONS(arr1.begin(), arr1.end(), expected.begin(), expected.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(arr2.begin(), arr2.end(), expected.begin(), expected.end());
+  }
+  {
+    std::vector<int> arr1, arr2;
+    for(auto& [n, i]: data1)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(int i = 0; i < (int)arr1.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr1[i] < arr1[i + 1]);
+    }
+    for(int i = 0; i < (int)arr2.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr2[i] < arr2[i + 1]);
+    }
+    arr1.clear();
+    arr2.clear();
+    for(auto& [n, i]: data2)
+    {
+      if(n == 0)
+      {
+        arr1.push_back(i);
+      }
+      else
+      {
+        arr2.push_back(i);
+      }
+    }
+    for(int i = 0; i < (int)arr1.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr1[i] < arr1[i + 1]);
+    }
+    for(int i = 0; i < (int)arr2.size() - 1; ++i)
+    {
+      BOOST_CHECK(arr2[i] < arr2[i + 1]);
+    }
+  }
 }
 
--- LockBasedDataStructure/Queue/ThreadSafeQueue.hpp
+++ LockBasedDataStructure/Queue/ThreadSafeQueue.hpp
@@ -13,21 +13,34 @@ private:
     std::shared_ptr<T> mData;
     std::unique_ptr<Node> mNext;
   };
-  std::mutex mHeadLock;
+  mutable std::mutex mHeadLock;
   std::unique_ptr<Node> mHead;
-  std::mutex mTailLock;
+  mutable std::mutex mTailLock;
   Node* mTail;
+  std::condition_variable mCond;
 private:
   template <typename U>
   void pushImpl(U&& val);
   Node* getTail();
-  std::unique_ptr<T> popHead();
+  std::unique_ptr<Node> popHead();
+  std::unique_lock<std::mutex> waitForData();
+  std::unique_ptr<Node> waitPopHead();
+  std::unique_ptr<Node> waitPopHead(T& val);
+  std::unique_ptr<Node> tryPopHead();
+  std::unique_ptr<Node> tryPopHead(T& val);
 public:
+  ThreadSafeQueue();
   std::shared_ptr<T> tryPop();
+  bool tryPop(T& val);
+  std::shared_ptr<T> waitAndPop();
+  void waitAndPop(T& val);
   void push(T&& val);
   void push(const T& val);
+  bool empty() const;
 };
 
+template <typename T>
+ThreadSafeQueue<T>::ThreadSafeQueue(): mHead(std::make_unique<Node>()), mTail(mHead.get()) {}
 template <typename T>
 typename ThreadSafeQueue<T>::Node* ThreadSafeQueue<T>::getTail()
 {
@@ -35,23 +48,77 @@ typename ThreadSafeQueue<T>::Node* ThreadSafeQueue<T>::getTail()
   return mTail;
 }
 template <typename T>
-std::unique_ptr<T> ThreadSafeQueue<T>::popHead()
+std::unique_ptr<typename ThreadSafeQueue<T>::Node> ThreadSafeQueue<T>::popHead()
 {
-  std::lock_guard lk(mHeadLock);
-  auto tail = getTail();
-  if(mHead.get() == tail)
-  {
-    return nullptr;
-  }
   auto oldHead = std::move(mHead);
   mHead = std::move(oldHead->mNext);
   return oldHead;
 }
 template <typename T>
+std::unique_lock<std::mutex> ThreadSafeQueue<T>::waitForData()
+{
+  std::unique_lock lk(mHeadLock);
+  mCond.wait(lk, [this](){ return mHead.get() != getTail(); });
+  return lk;
+}
+template <typename T>
+std::unique_ptr<typename ThreadSafeQueue<T>::Node> ThreadSafeQueue<T>::waitPopHead()
+{
+  auto lk = waitForData();
+  return popHead();
+}
+template <typename T>
+std::unique_ptr<typename ThreadSafeQueue<T>::Node> ThreadSafeQueue<T>::waitPopHead(T& val)
+{
+  auto lk = waitForData();
+  // copy data before popHead for exception safety.
+  // if popHead is executed before move assignment, the poped value is lost in case where this move assignment throws an exception
+  val = std::move(*mHead->mData);
+  return popHead();
+}
+template <typename T>
+std::unique_ptr<typename ThreadSafeQueue<T>::Node> ThreadSafeQueue<T>::tryPopHead()
+{
+  std::lock_guard lk(mHeadLock);
+  if(mHead.get() == getTail())
+  {
+    return nullptr;
+  }
+  return popHead();
+}
+template <typename T>
+std::unique_ptr<typename ThreadSafeQueue<T>::Node> ThreadSafeQueue<T>::tryPopHead(T& val)
+{
+  std::lock_guard lk(mHeadLock);
+  if(mHead.get() == getTail())
+  {
+    return nullptr;
+  }
+  val = std::move(*mHead->mData);
+  return popHead();
+}
+template <typename T>
 std::shared_ptr<T> ThreadSafeQueue<T>::tryPop()
 {
-  auto res = popHead();
-  return res ? res->mData : nullptr;
+  auto head = tryPopHead();
+  return head ? head->mData : nullptr;
+}
+template <typename T>
+bool ThreadSafeQueue<T>::tryPop(T& val)
+{
+  auto head = tryPopHead(val);
+  return head;
+}
+template <typename T>
+std::shared_ptr<T> ThreadSafeQueue<T>::waitAndPop()
+{
+  auto head = waitPopHead();
+  return head->mData;
+}
+template <typename T>
+void ThreadSafeQueue<T>::waitAndPop(T& val)
+{
+  auto head = waitPopHead(val);
 }
 template <typename T>
 template <typename U>
@@ -59,10 +126,13 @@ void ThreadSafeQueue<T>::pushImpl(U&& val)
 {
   auto data = std::make_shared<T>(std::forward<U>(val));
   auto newNode = std::make_unique<Node>();
-  std::lock_guard lk(mTailLock);
-  mTail->mData = std::move(data);
-  mTail->next = std::move(newNode);
-  mTail = mTail->next.get();
+  {
+    std::lock_guard lk(mTailLock);
+    mTail->mData = std::move(data);
+    mTail->mNext = std::move(newNode);
+    mTail = mTail->mNext.get();
+  }
+  mCond.notify_one();
 }
 template <typename T>
 void ThreadSafeQueue<T>::push(T&& val)
@@ -73,5 +143,11 @@ template <typename T>
 void ThreadSafeQueue<T>::push(const T& val)
 {
   pushImpl(val);
+}
+template <typename T>
+bool ThreadSafeQueue<T>::empty() const
+{
+  std::lock_guard lk(mHeadLock);
+  return mHead.get() == getTail();
 }
 
